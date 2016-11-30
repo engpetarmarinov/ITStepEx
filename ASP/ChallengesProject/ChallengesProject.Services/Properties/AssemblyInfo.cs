57621af15fe8e9d20a22de2dc7106d095408ceec
Petar Marinov <eng.petar.marinov@gmail.com>
2016-11-30 17:04:58 +0200

    ChallengesProject: Move services

--- ASP/ChallengesProject/ChallengesProject.Services/Properties/AssemblyInfo.cs
+++ ASP/ChallengesProject/ChallengesProject.Services/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Services")]
+[assembly: AssemblyTitle("ChallengesProject.Services")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Services")]
+[assembly: AssemblyProduct("ChallengesProject.Services")]
 [assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
