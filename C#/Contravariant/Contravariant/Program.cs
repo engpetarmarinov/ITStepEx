538b55180ce0db326740c339837afb30ded7ef07
Petar Marinov <petarmarinov@uk365office.co.uk>
2016-12-21 18:17:34 +0200

    C#: Add Covariant and Contravariant examples for generic interfaces

---
+++ C#/Contravariant/Contravariant/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+namespace Contravariant
+{
+    // Contravariant interface.
+    interface IContravariant<in A> { }
+
+    // Extending contravariant interface.
+    interface IExtContravariant<in A> : IContravariant<A> { }
+
+    // Implementing Contravariant interface.
+    class SampleContravariant<A> : IContravariant<A> { }
+
+    interface ICovariant<out A> { }
+
+    // Extending Covariant interface.
+    interface IExtCovariant<out A> : ICovariant<A> { }
+
+    // Implementing Covariant interface.
+    class SampleICovariant<A> : ICovariant<A> { }
+
+    class Program
+    {
+        static void Main()
+        {
+            IContravariant<Object> iobj = new SampleContravariant<Object>();
+            IContravariant<String> istr = new SampleContravariant<String>();
+
+            // You can assign iobj to istr because
+            // the IContravariant interface is contravariant.
+            // Allow implicit casting IContravariant<object> --> IContravariant<string>
+            istr = iobj;
+
+            ICovariant<Object> iobjtest = new SampleICovariant<object>();
+            ICovariant<String> istrtest = new SampleICovariant<string>();
+
+            //Allow implicit casting ICovariant<string> --> ICovariant<object>
+            iobjtest = istrtest;
+        }
+    }
+}
